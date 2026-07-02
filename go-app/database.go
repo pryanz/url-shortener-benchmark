38797dfa770756dc9e6e57d0021670b62fe90cc0
Priyansh Khare <priyanshkhare2005@gmail.com>
2026-07-02 16:08:58 +0530

    feat: implement optimized docker blueprints for golang and python app

--- go-app/database.go
+++ go-app/database.go
@@ -10,7 +10,7 @@ var Ctx = context.Background()
 
 func InitRedis(){
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: "redis-service:6379",
 		Password: "",
 		DB: 0,
 		Protocol: 2,
